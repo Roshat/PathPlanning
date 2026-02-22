f4b083b5c38b8a9e0b03497806fceb2d9ee9a66d
Roşhat <114759538+Roshat@users.noreply.github.com>
2026-02-22 15:59:27 +0300

    CreateRandomSolution.m

--- CreateRandomSolution.m
+++ CreateRandomSolution.m
@@ -1,15 +1,4 @@
-%
-% Copyright (c) 2015, Yarpiz (www.yarpiz.com)
-% All rights reserved. Please read the "license.txt" for license terms.
-%
-% Project Code: YPAP115
-% Project Title: Path Planning using PSO in MATLAB
-% Publisher: Yarpiz (www.yarpiz.com)
-% 
-% Developer: S. Mostapha Kalami Heris (Member of Yarpiz Team)
-% 
-% Contact Info: sm.kalami@gmail.com, info@yarpiz.com
-%
+
 
 function sol1=CreateRandomSolution(model)
 
@@ -24,4 +13,5 @@ function sol1=CreateRandomSolution(model)
     sol1.x=unifrnd(xmin,xmax,1,n);
     sol1.y=unifrnd(ymin,ymax,1,n);
     
+
 end
